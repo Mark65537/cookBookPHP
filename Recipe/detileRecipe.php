710e89df23c0b2c3e65e00d1ae4d432b642a2eac
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2022-10-02 19:55:16 +0700

    добавлена страница с деталями рецепта

--- Recipe/detileRecipe.php
+++ Recipe/detileRecipe.php
@@ -1,2 +1,34 @@
 <?php
+    include 'opendb.php';
+    include 'layout.php';
+
+    if(!empty($_GET) && isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $data = $pdo->query('SELECT * FROM recipe_models WHERE id=' . $id);
+        $result = $data->fetch(PDO::FETCH_OBJ);
+        $foto = $result->foto;
+        $name = $result->name;
+        $ingredients = $result->ingredients;
+        $steps = $result->steps;
+        echo <<< END
+        <h1>Detile Recipe</h1>
+
+
+
+        <div class='bg-light shadow-sm mx-auto' style='width: 80%; height: 200px; border-radius: 21px 21px 0 0;'>
+                    <image class='recipeImg' src='/images/$foto'>
+                    <span>
+                        <label>Recipe name: <b>$name</b>
+                        <br>Ingredients: $ingredients
+                        <br>Steps: $steps</label>
+                    </span>
+        </div><p>
+END;
+
+    }else{
+        echo "Error to show detiles";
+    }
+?>
+
+
 
